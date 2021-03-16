3690c172a21b32d9b2a7cc21c2ddadd3f892071f
Danil <bifidokk@gmail.com>
2021-03-16 19:47:55 +0200

    Move command resolver to the separated service

--- src/Service/Webhook/WebhookService.php
+++ src/Service/Webhook/WebhookService.php
@@ -4,16 +4,11 @@ declare(strict_types=1);
 
 namespace App\Service\Webhook;
 
-use App\Entity\User;
-use App\Service\Command\AddCustomHabitCommand;
 use App\Service\Command\CommandInterface;
-use App\Service\Command\CommandName;
+use App\Service\Command\CommandResolver;
 use App\Service\User\UserService;
-use App\Service\User\UserState;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\DependencyInjection\ServiceLocator;
-use TgBotApi\BotApiBase\Type\MessageEntityType;
-use TgBotApi\BotApiBase\Type\MessageType;
 use TgBotApi\BotApiBase\Type\UpdateType;
 
 class WebhookService
@@ -22,17 +17,20 @@ class WebhookService
     private ServiceLocator $commandLocator;
     private LoggerInterface $logger;
     private UserService $userService;
+    private CommandResolver $commandResolver;
 
     public function __construct(
         string $token,
         ServiceLocator $commandLocator,
         LoggerInterface $logger,
-        UserService $userService
+        UserService $userService,
+        CommandResolver $commandResolver
     ) {
         $this->token = $token;
         $this->commandLocator = $commandLocator;
         $this->logger = $logger;
         $this->userService = $userService;
+        $this->commandResolver = $commandResolver;
     }
 
     public function isTokenValid(string $token): bool
@@ -52,7 +50,7 @@ class WebhookService
             return;
         }
 
-        $commandName = $this->resolveCommand($update->message, $user);
+        $commandName = $this->commandResolver->resolve($update->message, $user);
 
         if ($commandName === null) {
             return;
@@ -65,54 +63,5 @@ class WebhookService
         } catch (\Throwable $e) {
             $this->logger->error($e->getMessage(), $e->getTrace());
         }
-    }
-
-    private function resolveCommand(MessageType $message, User $user): ?string
-    {
-        if (isset($message->entities) && $message->entities !== null) {
-            return $this->parseCommandFromMessage($message);
-        }
-
-        $command = $this->resolveCommandByName($message);
-
-        if ($command !== null) {
-            return $command;
-        }
-
-        return $this->resolveCommandByUserState($user);
-    }
-
-    private function parseCommandFromMessage(MessageType $message): ?string
-    {
-        if ($message->entities === null) {
-            return null;
-        }
-
-        foreach ($message->entities as $entity) {
-            if ($entity->type !== MessageEntityType::TYPE_BOT_COMMAND) {
-                continue;
-            }
-
-            $command = substr($message->text, $entity->offset, $entity->length);
-
-            return substr($command, 1);
-        }
-
-        return null;
-    }
-
-    private function resolveCommandByName(MessageType $message): ?string
-    {
-        return CommandName::getName($message->text);
-    }
-
-    private function resolveCommandByUserState(User $user): ?string
-    {
-        switch ($user->getState()) {
-            case UserState::NEW_CUSTOM_HABIT:
-                return AddCustomHabitCommand::COMMAND_NAME;
-        }
-
-        return null;
     }
 }
