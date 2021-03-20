9174f593d662dfc099846145a8c03733186c134b
Danil <bifidokk@gmail.com>
2021-03-20 18:45:21 +0200

    Remove dependencies

--- src/Service/Command/BackCommand.php
+++ src/Service/Command/BackCommand.php
@@ -5,30 +5,18 @@ declare(strict_types=1);
 namespace App\Service\Command;
 
 use App\Entity\User;
-use App\Service\User\UserService;
-use Psr\Log\LoggerInterface;
 use Symfony\Component\DependencyInjection\ServiceLocator;
-use TgBotApi\BotApiBase\BotApiComplete;
 use TgBotApi\BotApiBase\Type\MessageType;
 
 class BackCommand implements CommandInterface
 {
     public const COMMAND_NAME = 'back';
 
-    private BotApiComplete $bot;
-    private LoggerInterface $logger;
-    private UserService $userService;
     private ServiceLocator $commandLocator;
 
     public function __construct(
-        BotApiComplete $bot,
-        LoggerInterface $logger,
-        UserService $userService,
         ServiceLocator $commandLocator
     ) {
-        $this->bot = $bot;
-        $this->logger = $logger;
-        $this->userService = $userService;
         $this->commandLocator = $commandLocator;
     }
 
