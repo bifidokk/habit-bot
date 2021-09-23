e68b9d9b4e524f9fb64d0eac52d6c3a79f0854ab
bifidokk <bifidokk@gmail.com>
2021-09-23 20:48:04 +0300

    Fix translator method

--- src/Translator/NoTranslator.php
+++ src/Translator/NoTranslator.php
@@ -4,12 +4,16 @@ declare(strict_types=1);
 
 namespace App\Translator;
 
+use Symfony\Component\Translation\MessageCatalogue;
 use Symfony\Component\Translation\TranslatorBagInterface;
 use Symfony\Contracts\Translation\LocaleAwareInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 class NoTranslator implements TranslatorInterface, TranslatorBagInterface, LocaleAwareInterface
 {
+    /** @var MessageCatalogue[] */
+    private $catalogues = [];
+
     public function trans(string $id, array $parameters = [], string $domain = null, string $locale = null)
     {
         return $id;
@@ -27,5 +31,13 @@ class NoTranslator implements TranslatorInterface, TranslatorBagInterface, Local
     public function getLocale()
     {
         return '';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCatalogues(): array
+    {
+        return array_values($this->catalogues);
     }
 }
