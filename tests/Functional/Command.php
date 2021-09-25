50cb8040d204550d8a267252edb8ed6dd391284e
bifidokk <bifidokk@gmail.com>
2021-09-25 18:40:28 +0300

    Add DAMADoctrineTestBundle

--- tests/Functional/Command.php
+++ tests/Functional/Command.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace App\Tests\Functional;
 
-use Doctrine\Common\DataFixtures\Purger\ORMPurger;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use TgBotApi\BotApiBase\BotApiComplete;
@@ -14,7 +12,6 @@ class Command extends WebTestCase
 {
     protected BotApiComplete $botApiCompleteMock;
     protected KernelBrowser $client;
-    private EntityManagerInterface $entityManager;
 
     protected function setUp(): void
     {
@@ -25,30 +22,6 @@ class Command extends WebTestCase
         $this->client = static::createClient();
         $this->client->getContainer()->set('TgBotApi\BotApiBase\BotApiComplete', $this->botApiCompleteMock);
         $this->client->disableReboot();
-
-        $this->entityManager = $this->client->getContainer()
-            ->get('doctrine')
-            ->getManager();
-    }
-
-    protected function tearDown(): void
-    {
-        parent::tearDown();
-
-        $connection = $this->entityManager->getConnection();
-        $databasePlatform = $connection->getDatabasePlatform();
-
-        if ($databasePlatform->supportsForeignKeyConstraints()) {
-            $connection->executeQuery('SET FOREIGN_KEY_CHECKS=0');
-        }
-
-        $purger = new ORMPurger($this->entityManager);
-        $purger->setPurgeMode(2);
-        $purger->purge();
-
-        if ($databasePlatform->supportsForeignKeyConstraints()) {
-            $connection->executeQuery('SET FOREIGN_KEY_CHECKS=1');
-        }
     }
 
     protected function sendRequest(string $content): void
