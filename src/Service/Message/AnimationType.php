cef37bd5c80da06f69c5841cc9c29d439a97ba97
bifidokk <bifidokk@gmail.com>
2023-01-04 22:01:07 +0200

    Fix ecs

--- src/Service/Message/AnimationType.php
+++ src/Service/Message/AnimationType.php
@@ -7,10 +7,16 @@ namespace App\Service\Message;
 enum AnimationType: string
 {
     case Success = 'success';
+
     case Timezone = 'timezone';
+
     case Language = 'language';
+
     case NotRemoved = 'not_removed';
+
     case Removed = 'removed';
+
     case HabitDone = 'habit_done';
+
     case HabitBusy = 'habit_busy';
 }
