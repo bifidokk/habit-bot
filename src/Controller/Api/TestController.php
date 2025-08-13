5c08bc59baa85f5caf201b25bf16065346df1fe8
bifidokk <bifidokk@gmail.com>
2025-08-13 17:44:37 +0300

    Fix phpstan

--- src/Controller/Api/TestController.php
+++ src/Controller/Api/TestController.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace App\Controller\Api;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,11 +12,11 @@ use Symfony\Component\Security\Http\Attribute\CurrentUser;
 class TestController extends AbstractController
 {
     #[Route('/api/test', name: 'api_test', methods: ['GET'])]
-    public function index(#[CurrentUser] $user): JsonResponse
+    public function index(#[CurrentUser] ?User $user): JsonResponse
     {
         return $this->json([
             'message' => 'You are authenticated!',
-            'user' => $user ? $user->getUserIdentifier() : null,
+            'user' => $user?->getUserIdentifier(),
             'roles' => $user ? $user->getRoles() : [],
         ]);
     }
--- src/EventListener/Security/JWTCreatedListener.php
+++ src/EventListener/Security/JWTCreatedListener.php
@@ -3,12 +3,14 @@ declare(strict_types=1);
 
 namespace App\EventListener\Security;
 
+use App\Entity\User;
 use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
 
 class JWTCreatedListener
 {
     public function onJWTCreated(JWTCreatedEvent $event): void
     {
+        /** @var User $user */
         $user = $event->getUser();
         $payload = $event->getData();
 
