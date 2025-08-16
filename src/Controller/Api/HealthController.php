ab39d77bb126e1c3522b207fc705a3c6aa4e3d4a
bifidokk <bifidokk@gmail.com>
2025-08-16 23:14:48 +0300

    Add health endpoint, add CORS rules

---
+++ src/Controller/Api/HealthController.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller\Api;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HealthController extends AbstractController
+{
+    #[Route('/api/health', name: 'api_health', methods: ['GET'])]
+    public function index(): JsonResponse
+    {
+        return $this->json([]);
+    }
+}
